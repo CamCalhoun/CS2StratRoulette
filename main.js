16d85e9fd8f87d088039dbed2bcb0ee27b621b5a
Cameron Calhoun <cameron.calhoun@gmail.com>
2024-10-23 00:48:27 -0400

    converted index to tailwind (keeping styles.css for now), added minwindow size thru electron and changed default, added more breakpoints in stratRoulette

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ const { app, BrowserWindow, ipcMain } = require('electron/main')
 const path = require('node:path')
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-	webPreferences: {
-		preload: path.join(__dirname, 'preload.js')}
-  })
+	const win = new BrowserWindow({
+		width: 1000,
+		height: 750,
+		minWidth: 1000,
+		minHeight: 750,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js')
+		}
+	})
 
-  win.loadFile('index.html')
+	win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
