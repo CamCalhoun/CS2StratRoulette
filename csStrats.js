a1c4e75b9938095734f64a34c2c5affabae2c8ec
Cameron Calhoun <cameron.calhoun01@gmail.com>
2024-10-28 12:54:00 -0400

    began logic

---
+++ csStrats.js
@@ -0,0 +1,148 @@
+const maps = ["any", "Dust2", "Mirage", "Nuke", "Inferno", "Vertigo", "Ancient", "Anubis"]
+const buys = ["Eco", "Half", "Full"]
+const sides = ["any", "CT", "T"]
+
+const strats = {
+	"any": {
+		"Eco": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Half": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Full": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+
+	},
+	"Dust2": {
+		"Eco": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Half": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Full": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+
+	},
+	"Mirage": {
+		"Eco": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Half": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Full": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+
+	},
+	"Nuke": {
+		"Eco": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Half": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Full": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+
+	},
+	"Inferno": {
+		"Eco": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Half": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Full": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+	},
+	"Vertigo": {
+		"Eco": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Half": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Full": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+	},
+	"Ancient": {
+		"Eco": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Half": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Full": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+	},
+	"Anubis": {
+		"Eco": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Half": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+		"Full": {
+			"any": ["PlaceholderStrats"],
+			"CT": ["PlaceholderStrats"],
+			"T": ["PlaceholderStrats"],
+		},
+	},
+
+}
+export { maps, buys, sides, strats };
---
+++ stratRoulette.js
@@ -0,0 +1,5 @@
+import { maps, buys, sides, strats } from 'csStrats.js'
+function reroll() {
+	document.getElementById("displayStratTitle").innerText = "Test1"
+	document.getElementById("displayStratInfo").innerText = "Test2"
+}
